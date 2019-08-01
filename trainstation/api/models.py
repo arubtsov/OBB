2a8718a797db75d9180fa383727ee5319641ec44
Rb3As <arseniy.rubtsov@gmail.com>
2019-08-01 15:47:22 +0300

    simple models definitions

--- trainstation/api/models.py
+++ trainstation/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class TrainSection(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class TrainStation(models.Model):
+    name = models.CharField(max_length=30)
