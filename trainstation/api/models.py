4ed25bb51fb8d593a0b38181cf0dd336818da1fb
Rb3As <arseniy.rubtsov@gmail.com>
2019-08-03 12:26:33 +0300

    introduce NamedEntity abstract model

--- trainstation/api/models.py
+++ trainstation/api/models.py
@@ -1,35 +1,31 @@
 from django.db import models
 
 
-class TrainStation(models.Model):
+class NamedEntity(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
 
-
-class Train(models.Model):
-    name = models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.name
+    class Meta:
+        abstract = True
 
 
-class Platform(models.Model):
-    name = models.CharField(max_length=30)
+class TrainStation(NamedEntity):
+    pass
+
+
+class Train(NamedEntity):
+    pass
+
+
+class Platform(NamedEntity):
     train = models.OneToOneField(Train, on_delete=models.SET_NULL)
     station = models.ForeignKey(TrainStation, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
 
-
-class TrainSection(models.Model):
-    name = models.CharField(max_length=30)
+class TrainSection(NamedEntity):
     train = models.ForeignKey(Train, on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return self.name
 
 
 class Person(models.Model):
