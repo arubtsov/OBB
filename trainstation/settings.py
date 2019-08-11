93b4b30871d342ff39a52155541f7022b346c0da
Rb3As <arseniy.rubtsov@gmail.com>
2019-08-11 20:58:50 +0300

    add api to installed applications

--- trainstation/settings.py
+++ trainstation/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'trainstation.api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
