3793f04273864aab8d9c21c478e324288f28d190
Rb3As <arseniy.rubtsov@gmail.com>
2019-08-12 19:50:56 +0300

    add PersonViewSet

--- trainstation/api/views.py
+++ trainstation/api/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from trainstation.api.models import Train
-from trainstation.api.serializers import TrainSerializer
+from trainstation.api.models import Train, Person
+from trainstation.api.serializers import TrainSerializer, PersonSerializer
 
 
 class TrainViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows trains to be viewed or edited.
     """
     queryset = Train.objects.all()
     serializer_class = TrainSerializer
     
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows persons to be viewed or edited.
+    """
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
--- trainstation/urls.py
+++ trainstation/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.urls import include, path
 from rest_framework import routers
-from trainstation.api.views import TrainViewSet
+from trainstation.api.views import TrainViewSet, PersonViewSet
 
 router = routers.DefaultRouter()
 router.register(r'trains', TrainViewSet)
+router.register(r'persons', PersonViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
