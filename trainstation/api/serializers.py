0895686b14e5a1d3e3badd58ae6bc66e762c3017
Rb3As <arseniy.rubtsov@gmail.com>
2019-08-12 19:46:46 +0300

    add Person Serializer

--- trainstation/api/serializers.py
+++ trainstation/api/serializers.py
@@ -1,7 +1,12 @@
-from trainstation.api.models import Train
+from trainstation.api.models import Train, Person
 from rest_framework import serializers
 
 class TrainSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Train
         fields = ['name', 'url']
+
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Person
+        fields = ['url', 'first_name', 'last_name']
