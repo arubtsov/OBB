8da4e71d5ec0ca1b6102f83c82393e51af4aac1e
Rb3As <arseniy.rubtsov@gmail.com>
2019-08-18 14:32:31 +0300

    restore admin access

--- trainstation/urls.py
+++ trainstation/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from trainstation.api.views import TrainViewSet, PersonViewSet
@@ -22,6 +23,7 @@ router.register(r'trains', TrainViewSet)
 router.register(r'persons', PersonViewSet)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api/', include('rest_framework.urls', namespace='rest_framework'))
 ]
