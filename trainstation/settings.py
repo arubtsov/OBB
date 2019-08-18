ad5394c5fc04f4d062f0dd7c42d37bbaab1b8b9d
Rb3As <arseniy.rubtsov@gmail.com>
2019-08-18 15:19:42 +0300

    save queries logging configuration

--- trainstation/settings.py
+++ trainstation/settings.py
@@ -132,3 +132,19 @@ REST_FRAMEWORK = {
     ],
 }
 
+""" LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+} """
